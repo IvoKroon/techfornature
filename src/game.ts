3acbb3991f63d66c477ce04c3c6cb122b22bf5cf
Ivo Kroon <ivokroon@mbp-van-ivo.home>
2016-12-03 21:09:49 +0100

    did some changes

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,6 @@
 /// <reference path="../tsDefinitions/phaser.d.ts" />
+/// <reference path="./RunningState.ts" />
+
 class SimpleGame
 {
 	game:Phaser.Game;
@@ -9,12 +11,11 @@ class SimpleGame
 		this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
 		this.game.state.add("MenuScreenState", MenuScreenState , false);
 		this.game.state.add("RunningState", RunningState , false);
-		this.game.state.start("MenuScreenState",true,true);
-		
-		//this.game.state.start("RunningState",true,true);
-
+		this.game.state.add("ShopState", ShopState , false);
+		this.game.state.start("RunningState",true,true);
 	}
 }
+
 
 // when the page has finished loading, create our game
 window.onload = () => {
--- src/objects/ButtonObject.ts
+++ src/objects/ButtonObject.ts
@@ -1,13 +1,18 @@
 class ButtonObject extends Phaser.Button{
-    
+    keyString:string;
+    game:Phaser.Game;
     constructor(game:Phaser.Game, x:number, y:number, key:string, callback:Function){
         super(game,x,y,key, callback);
-        game.add.existing(this);
+        this.keyString = key;
     }
 
     setSizes(width:number, height:number){
         this.width = width;
         this.height = height;
+    }
+
+    render(){
+        this.game.add.existing(this);
     }
 
     action(){}
